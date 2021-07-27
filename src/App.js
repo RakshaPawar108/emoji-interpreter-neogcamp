45b0e69742e107179dfdcb644fdfc75dc0401d47
Raksha Pawar <rakshapawar10.sae@gmail.com>
2021-07-27 20:40:17 +0530

    chore:counter made just for testing, ignore please

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    likeCounter = likeCounter + 1;
+    setLikeCounter(likeCounter);
+  }
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <h1>Inside Out!!</h1>
+      <button onClick={likeClickHandler}>Like Me!</button>
+      <p>{likeCounter}</p>
     </div>
   );
 }
